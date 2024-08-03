8b23b8a2a5da642d8dfcfd1d401eed0849590109
PolkaS3 <polkashinoyama3@gmail.com>
2024-08-03 09:35:41 +0000

    hehehehe

--- rnd/autogpt_builder/next.config.js
+++ rnd/autogpt_builder/next.config.js
@@ -1,3 +1,14 @@
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+
+module.exports = {
+  webpack: (config) => {
+    config.plugins = [
+      ...config.plugins,
+      new NodePolyfillPlugin(),
+    ];
+    return config;
+  },
+};
 
 module.exports = {
   webpack: (config) => {
